19edff2c627586f44ed024b996373124f9aa39c2
Matt London <mattlondon57@gmail.com>
2021-11-06 14:52:55 -0400

    Constants file

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+//
+// Created by mattl on 11/6/2021.
+//
+
+#ifndef SUDOKU_SOLVER_CONSTANTS_H
+#define SUDOKU_SOLVER_CONSTANTS_H
+
+#define BOARD_LENGTH 9
+#define BLANK 0
+
+#endif //SUDOKU_SOLVER_CONSTANTS_H
