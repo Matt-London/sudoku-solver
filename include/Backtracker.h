3a9dab38216febdaa551c6db08e4089d312b9791
Matt London <mattlondon57@gmail.com>
2021-11-07 00:12:28 -0400

    Add Backtracker solver class

---
+++ include/Backtracker.h
@@ -0,0 +1,28 @@
+//
+// Created by mattl on 11/6/2021.
+//
+
+//
+// Structure of Backtracker borrowed and implemented from https://github.com/CSCI-140-242-CSAPX by RIT CS
+//
+
+#ifndef SUDOKU_SOLVER_BACKTRACKER_H
+#define SUDOKU_SOLVER_BACKTRACKER_H
+
+
+class Backtracker {
+    private:
+        bool debug;
+
+    public:
+        /**
+         * Constructor for backtracker
+         *
+         * @param debug whether debug should be enabled
+         */
+        Backtracker(bool debug);
+
+};
+
+
+#endif //SUDOKU_SOLVER_BACKTRACKER_H
---
+++ src/Backtracker.cpp
@@ -0,0 +1,15 @@
+//
+// Created by mattl on 11/6/2021.
+//
+
+#include "Backtracker.h"
+
+/**
+ * Constructor for backtracker
+ *
+ * @param debug whether debug should be enabled
+ */
+Backtracker::Backtracker(bool debug) {
+    this->debug = debug;
+    
+}
