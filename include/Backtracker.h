065b6133140c598fa91751965ba43f275973618b
Matt London <mattlondon57@gmail.com>
2021-11-07 00:41:49 -0400

    Implement solve function

--- include/Backtracker.h
+++ include/Backtracker.h
@@ -9,6 +9,11 @@
 #ifndef SUDOKU_SOLVER_BACKTRACKER_H
 #define SUDOKU_SOLVER_BACKTRACKER_H
 
+#include <iostream>
+#include <string>
+#include <optional>
+
+#include "SudokuConfig.h"
 
 class Backtracker {
     private:
@@ -21,6 +26,22 @@ class Backtracker {
          * @param debug whether debug should be enabled
          */
         Backtracker(bool debug);
+
+        /**
+         * If debug is on will print the config
+         *
+         * @param msg Status of the config
+         * @param config Printed version of config
+         */
+        void debugPrint(std::string msg, SudokuConfig &config);
+
+        /**
+         * Recursively look for solution
+         *
+         * @param config configuration to look at
+         * @return Possibly a solution or possibly empty
+         */
+        std::optional<SudokuConfig> solve(SudokuConfig config);
 
 };
 
--- src/Backtracker.cpp
+++ src/Backtracker.cpp
@@ -11,5 +11,48 @@
  */
 Backtracker::Backtracker(bool debug) {
     this->debug = debug;
-    
+
+}
+
+/**
+ * If debug is on will print the config
+ *
+ * @param msg Status of the config
+ * @param config Printed version of config
+ */
+void Backtracker::debugPrint(std::string msg, SudokuConfig &config) {
+    if (debug) {
+        std::cout << msg << ":\n" << config.printable() << std::endl;
+    }
+}
+
+/**
+ * Recursively look for solution
+ *
+ * @param config configuration to look at
+ * @return Possibly a solution or possibly empty
+ */
+std::optional<SudokuConfig> Backtracker::solve(SudokuConfig config) {
+    debugPrint("Current config", config);
+
+    // Check if complete
+    if (config.isGoal()) {
+        return std::optional(config);
+    }
+    else {
+        // Check each successor which are already known to be valid
+        for (SudokuConfig successor: config.getSuccessors()) {
+            debugPrint("\tValid successor", successor);
+            std::optional possibleSol = solve(successor);
+            if (possibleSol.has_value()) {
+                return possibleSol;
+            }
+
+        }
+
+    }
+
+    // If none of that runs that means there is no solution
+    return {};
+
 }
